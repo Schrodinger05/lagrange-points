61a3dd9615e86fe54a8dc12212f98143a718296f
Gabriel Machado Fernandes <gabrielmfernandes25@gmail.com>
2024-03-03 00:14:03 -0300

    fix arithmetic equation order in stability analisis

--- src/lagrange_points/stability.rs
+++ src/lagrange_points/stability.rs
@@ -31,27 +31,27 @@ fn calculate_uxx(values: &Vals) -> f64 {
     let m22 = m21;
 
     let res = 1.0
-        + 3.0 * (1.0 - 2.0 * m) * (-d1 / 2.0 + 2.0 * m + x).powi(2) / 2.0
-            * ((-d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * (1.0 - 2.0 * m) * (-d1 / 2.0 + 2.0 * m + x).powi(2)) / (2.0
+            * ((-d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        - (1.0 - 2.0 * m) / 2.0
-            * ((-d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0);
+        - (1.0 - 2.0 * m) / (2.0
+            * ((-d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0));
     let res = res
-        + 3.0 * (1.0 - 2.0 * m) * (d1 / 2.0 + 2.0 * m + x).powi(2) / 2.0
-            * ((d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * (1.0 - 2.0 * m) * (d1 / 2.0 + 2.0 * m + x).powi(2)) / (2.0
+            * ((d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        - (1.0 - 2.0 * m) / 2.0
-            * ((d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0);
+        - (1.0 - 2.0 * m) / (2.0
+            * ((d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0));
     let res = res
-        + 3.0 * m22 * (-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2)
-            / ((-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * m22 * (-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2))
+            / (((-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        - m22 / ((-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0);
+        - m22 / (((-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0));
     let res = res
-        + 3.0 * m21 * (-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2)
-            / ((-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * m21 * (-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2))
+            / (((-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        - m21 / ((-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0);
+        - m21 / (((-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0));
 
     res
 }
@@ -68,27 +68,27 @@ fn calculate_uyy(values: &Vals) -> f64 {
     let m22 = m21;
 
     let res = 1.0
-        + 3.0 * (1.0 - 2.0 * m) * y.powi(2) / 2.0
-            * ((-d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * (1.0 - 2.0 * m) * y.powi(2)) / (2.0
+            * ((-d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        - (1.0 - 2.0 * m) / 2.0
-            * ((-d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0);
+        - (1.0 - 2.0 * m) / (2.0
+            * ((-d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0));
     let res = res
-        + 3.0 * (1.0 - 2.0 * m) * y.powi(2) / 2.0
-            * ((d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * (1.0 - 2.0 * m) * y.powi(2)) / (2.0
+            * ((d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        - (1.0 - 2.0 * m) / 2.0
-            * ((d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0);
+        - (1.0 - 2.0 * m) / (2.0
+            * ((d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0));
     let res = res
-        + 3.0 * m22 * y.powi(2)
-            / ((-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * m22 * y.powi(2))
+            / (((-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        - m22 / ((-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0);
+        - m22 / (((-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0));
     let res = res
-        + 3.0 * m21 * y.powi(2)
-            / ((-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * m21 * y.powi(2))
+            / (((-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        - m21 / ((-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0);
+        - m21 / (((-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(3.0 / 2.0));
 
     res
 }
@@ -104,17 +104,17 @@ fn calculate_uxy(values: &Vals) -> f64 {
     let m21 = m;
     let m22 = m21;
 
-    let res = 3.0 * (1.0 - 2.0 * m) * (-d1 / 2.0 + 2.0 * m + x) * y / 2.0
-        * ((-d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+    let res = (3.0 * (1.0 - 2.0 * m) * (-d1 / 2.0 + 2.0 * m + x) * y) / (2.0
+        * ((-d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        + 3.0 * (1.0 - 2.0 * m) * (d1 / 2.0 + 2.0 * m + x) * y / 2.0
-            * ((d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * (1.0 - 2.0 * m) * (d1 / 2.0 + 2.0 * m + x) * y) / (2.0
+            * ((d1 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        + 3.0 * m22 * (-1.0 - d2 / 2.0 + 2.0 * m + x) * y
-            / ((-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * m22 * (-1.0 - d2 / 2.0 + 2.0 * m + x) * y)
+            / (((-1.0 - d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
     let res = res
-        + 3.0 * m21 * (-1.0 + d2 / 2.0 + 2.0 * m + x) * y
-            / ((-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0);
+        + (3.0 * m21 * (-1.0 + d2 / 2.0 + 2.0 * m + x) * y)
+            / (((-1.0 + d2 / 2.0 + 2.0 * m + x).powi(2) + y.powi(2) + z.powi(2)).powf(5.0 / 2.0));
 
     res
 }
