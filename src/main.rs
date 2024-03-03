8b891128e19e703c62032ef5ac02dd3f70537cec
Gabriel Machado Fernandes <gabrielmfernandes25@gmail.com>
2024-03-03 00:14:45 -0300

    add calculation functions to main for release

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod lagrange_points;
 
 fn main() {
+    lagrange_points::calculate_l1();
     lagrange_points::calculate_l2();
+    lagrange_points::calculate_l3();
+    lagrange_points::calculate_l4();
+    lagrange_points::calculate_l5();
 }
